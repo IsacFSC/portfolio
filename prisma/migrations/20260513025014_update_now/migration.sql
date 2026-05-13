faee73dd98606861ac29f742ac07f66d076c1a79
isacFSC <isacktoo9@gmail.com>
2026-05-12 23:59:15 -0400

    fix: schema prisma

---
+++ prisma/migrations/20260513025014_update_now/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ContactLead" ALTER COLUMN "updatedAt" SET DEFAULT CURRENT_TIMESTAMP;
---
+++ prisma/migrations/20260513025549_change_id_to_native_uuid_default_n8n/migration.sql
@@ -0,0 +1,2 @@
+-- AlterTable
+ALTER TABLE "ContactLead" ALTER COLUMN "id" SET DEFAULT gen_random_uuid();
